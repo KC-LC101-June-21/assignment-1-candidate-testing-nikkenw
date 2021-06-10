ee1653345726ae5608055c4295fe46593fe0d794
papatstubbs <84359327+nikkenw@users.noreply.github.com>
2021-06-10 22:02:55 +0000

    updated assignment with loops and additional questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,32 +8,40 @@ let candidateName ;
 let question = "Who was the first American woman in space?";
 let correctAnswer = "Sally Ride";
 let candidateAnswer ;
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [question, "True or False: 5 kilometers == 5000 meters", "(5+3)/2*10=?", "Given the array[8, 'Orbit', 'Trajectory', 45], what entry is at index 2?", "What is the minimum crew size for the ISS?"];
+let correctAnswers = [correctAnswer, "true", "40", "Trajectory", "3"];
+let candidateAnswers = [];
 
 
 function askForName() {
-  // TODO 1.1b: Ask for candidate's name //
+  // TODO 1.1b: Ask for candidades name.
 candidateName = input.question("What is your name?");
 
 }
 
-function askQuestion() {
+//function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
-}
-function gradeQuiz(candidateAnswers) {
+  //};
+
+  for(let i = 0; i <= questions.length - 1; i++){
+    candidateAnswers.push(input.question(questions[i]));
+  };
+
+
+
+//function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-if(candidateAnswer === correctAnswer){
-  console.log("Correct answer!")
-} else {console.log("Incorrect")}
-  let grade;
-  
+//if(candidateAnswer === correctAnswer){
+  //console.log("Correct answer!")
+//} else {console.log("Incorrect")}
+  //let grade;
+  //return grade;
+//}
+for(let i = 0; i <= correctAnswers.length; i++){
+  console.log(`Correct Answer: ${correctAnswers[i]}. User Answer:${candidateAnswers[i]}`)
+};
 
-  return grade;
-}
 
 function runProgram() {
   askForName();
@@ -56,4 +64,4 @@ module.exports = {
   candidateAnswers: candidateAnswers,
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
-};
+}; 
